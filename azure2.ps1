88dbe5e87574ac6e53df641f2b3a9f364a71ca89
Ben Hollamby <ben.hollamby@outlook.com>
2022-08-31 12:00:48 +1200

    vnet and subnets

--- azure2.ps1
+++ azure2.ps1
@@ -27,6 +27,20 @@ function New-IASetup {
 
     PROCESS {
 
+        $AddressPrefix = Read-Host "Please enter the IPv4 address space: "
+        New-AzVirtualNetwork -Name VN_Core -ResourceGroupName RG_Networking -AddressPrefix $AddressPrefix -Location $Location | Out-Null
+        $sub_Protected = Read-Host "Please enter the subnet address for sub_Protected within $AddressPrefix range: "
+        New-AzVirtualNetworkSubnetConfig -Name sub_Protected -AddressPrefix $sub_Protected | Out-Null
+        $sub_External = Read-Host "Please enter the subnet address for sub_External within $AddressPrefix range: "
+        New-AzVirtualNetworkSubnetConfig -Name sub_Protected -AddressPrefix $sub_External | Out-Null
+        $sub_Internal = Read-Host "Please enter the subnet address for sub_Ixternal within $AddressPrefix range: "
+        New-AzVirtualNetworkSubnetConfig -Name sub_Protected -AddressPrefix $sub_Internal | Out-Null
+        $sub_Storage = Read-Host "Please enter the subnet address for sub_Storage within $AddressPrefix range: "
+        New-AzVirtualNetworkSubnetConfig -Name sub_Protected -AddressPrefix $sub_Storage | Out-Null
+        $sub_VirtualDesktop = Read-Host "Please enter the subnet address for sub_VirtualDesktop within $AddressPrefix range: "
+        New-AzVirtualNetworkSubnetConfig -Name sub_Protected -AddressPrefix $sub_VirtualDesktop | Out-Null
+        $sub_Server = Read-Host "Please enter the subnet address for sub_Server within $AddressPrefix range: "
+        New-AzVirtualNetworkSubnetConfig -Name sub_Protected -AddressPrefix $sub_Server | Out-Null
     }
 
     END {
