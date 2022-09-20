62895297ffa5c914c5584c16a4d6608f835381f4
Ben Hollamby <ben.hollamby@outlook.com>
2022-09-20 18:33:08 +1200

    fortigate issue wip

--- storage.ps1
+++ storage.ps1
@@ -1,25 +1,47 @@
 $VMLocalAdminUser = "fortigateuser"
-$VMLocalAdminSecurePassword = ConvertTo-SecureString "DEft0nes" -AsPlainText -Force
+$VMLocalAdminSecurePassword = ConvertTo-SecureString "password" -AsPlainText -Force
 $LocationName = "australiaeast"
 $ResourceGroupName = "RG_Networking"
 $ComputerName = "FGT-01"
-$VMName = "fgtvm"
-$VMSize = "Standard_F2s_v2"
+$VMName = (Get-AzVMImage -Location "australiaeast" -PublisherName "Fortinet" -Offer "fortinet_fortigate-vm_v5" -Skus "fortinet_fg-vm_payg_2022" -Version 7.2.1).Id
+$VMSize = "Standard_DS1_v2"
 
 $NIC = (Get-AzNetworkInterface).id[0]
 $INTNIC = (Get-AzNetworkInterface).id[1]
 $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
 
-$agreementTerms = Get-AzMarketplaceterms -Publisher "Fortinet" -Product "fortinet_fortigate-vm_v5" -Name "fortinet_fg-vm_payg_2022"
-Set-AzMarketplaceTerms -Publisher "Fortinet" -Product "fortinet_fortigate-vm_v5" -Name "fortinet_fg-vm_payg_2022" -Terms $agreementTerms -Accept
+$agreementterms = get-azmarketplaceterms -Publisher "Fortinet" -Product "fortinet_fortigate-vm_v5" -Name "fortinet_fg-vm"
+Set-AzMarketplaceTerms -Publisher "Fortinet" -Product "fortinet_fortigate-vm_v5" -Name "fortinet_fg-vm" -Terms $agreementTerms -Accept
 
 $VirtualMachine = New-AzVMConfig -VMName $VMName -VMSize $VMSize 
-$VirtualMachine = Set-AzVMPlan -VM $VirtualMachine -Publisher 'Fortinet' -Product 'fortinet_fortigate-vm_v5' -Name 'fortinet_fg-vm_payg_2022'
+$VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'Fortinet' -Offer -Skus 'fortinet_fg-vm' -Version '7.2.1'
+$VirtualMachine = Set-AzVMPlan -VM $VirtualMachine -Publisher 'Fortinet' -Product 'fortinet_fortigate-vm_v5' -Name 'fortinet_fg-vm'
 $VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Linux -ComputerName $ComputerName -Credential $Credential
 $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC -Primary
 $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $INTNIC
-$VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'Fortinet' -Offer 'fortinet_fortigate-vm_v5' -Skus 'fortinet_fg-vm_payg_2022' -Version '7.2.1'
 
-New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine
+
+
+New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine -
 
 #Get-AzVMImage -Location "australiaeast" -PublisherName "Fortinet" -Offer "fortinet_fortigate-vm_v5" -Skus "fortinet_fg-vm_payg_2022" -Version "latest"
+
+<#
+GENERAL
+Provisioning state Provisioning failed.
+Unable to deploy from the Marketplace image or a custom image sourced from Marketplace image. The part number in the purchase information for VM '/subscriptions/e4928e11-7de5-42cf-8972-85368ec589c2/resourceGroups/RG_Networking/providers/Microsoft.Compute/virtualMachines/fgtvm' is not as expected. Beware that the Plan object's properties are case-sensitive..
+VMMarketplaceInvalidInput
+Provisioning state error code ProvisioningState/failed/VMMarketplaceInvalidInput
+Guest agent Unknown
+DISKS
+fgtvm_OsDisk_1_496bf48ad29842eebb18546d1ad99d8c Provisioning failed.
+Unable to deploy from the Marketplace image or a custom image sourced from Marketplace image. The part number in the purchase information for VM '/subscriptions/e4928e11-7de5-42cf-8972-85368ec589c2/resourceGroups/RG_Networking/providers/Microsoft.Compute/virtualMachines/fgtvm' is not as expected. Beware that the Plan object's properties are case-sensitive..
+VMMarketplaceInvalidInput
+#>
+
+# LATEST ITERATION
+#Get-AzVMImage -Location australiaeast -PublisherName "fortinet" -Offer fortinet_fortigate-vm_v5 -Skus fortinet_fg-vm -Version 7.2.1
+#$agreementterms = get-azmarketplaceterms -Publisher "Fortinet" -Product "fortinet_fortigate-vm_v5" -Name "fortinet_fg-vm"
+#Set-AzMarketplaceTerms -Publisher "Fortinet" -Product "fortinet_fortigate-vm_v5" -Name "fortinet_fg-vm" -Terms $agreementTerms -Accept
+
+#Get-AzVMImage -Location "australiaeast" -PublisherName "Fortinet" -Offer "fortinet_fortigate-vm_v5" -Skus "fortinet_fg-vm_payg_2022" -Version 7.2.1
