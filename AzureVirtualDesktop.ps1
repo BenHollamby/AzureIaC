3759e7b0d6b8b6a95df4f85d5437c708f4ec6871
Ben Hollamby <ben.hollamby@outlook.com>
2022-09-29 16:31:45 +1300

    let arm template do the work and then edit post creation to match desired state

--- AzureVirtualDesktop.ps1
+++
@@ -1,7 +0,0 @@
-#Search through and remove all .rdpw files
-Get-ChildItem C:\ -Recurse -ErrorAction SilentlyContinue | Where-Object name -like *.rdpw | Remove-Item -Force
-#Add Shortcut to Desktop
-$Shell = New-Object -ComObject ("WScript.Shell")
-$Favorite = $Shell.CreateShortcut($env:PUBLIC + "\Desktop\Azure Virtual Desktop.url")
-$Favorite.TargetPath = "https://client.wvd.microsoft.com/arm/webclient/index.html";
-$Favorite.Save()
